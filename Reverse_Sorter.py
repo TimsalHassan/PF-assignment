6e044933d58282ca53cc3a102759845ac0fe4152
TimsalHassan <166032771+TimsalHassan@users.noreply.github.com>
2024-04-04 22:48:26 +0500

    Create Reverse_Sorter.py

---
+++ Reverse_Sorter.py
@@ -0,0 +1,8 @@
+#Name : Timsal Hassan
+#Roll Number : 1129
+#Semester : 1st 
+#Section : 3M
+Int=str(input('Write your number!'))
+reversed_integer=Int[::-1]
+spaced=' '.join(reversed_integer)
+print(spaced)
