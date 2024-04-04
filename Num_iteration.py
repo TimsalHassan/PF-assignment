ad6b8ce40c91ac7e479eeacd6c37ac2aca58f54f
TimsalHassan <166032771+TimsalHassan@users.noreply.github.com>
2024-04-04 22:43:37 +0500

    Create Num_iteration.py

---
+++ Num_iteration.py
@@ -0,0 +1,9 @@
+#Name : Timsal Hassan
+#Roll Number : 1129
+#Semester : 1st 
+#Section : 3M
+list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+for i in range(1, 11):
+    previous_number = i - 1
+    current_number = i
+    print('\033[43m',previous_number + current_number,'\033[0m')
