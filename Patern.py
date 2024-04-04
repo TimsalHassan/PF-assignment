db6ff9c470c1b6d74116172a846678f71829291a
TimsalHassan <166032771+TimsalHassan@users.noreply.github.com>
2024-04-04 22:46:26 +0500

    Create Patern.py

---
+++ Patern.py
@@ -0,0 +1,9 @@
+#Name : Timsal Hassan
+#Roll Number : 1129
+#Semester : 1st 
+#Section : 3M
+print(1)
+print(2,' ',2)
+print(3,' ',3,' ',3)
+print(4,' ',4,' ',4,' ',4)
+print(5,' ',5,' ',5,' ',5,' ',5)
