ee9834a8c7e5d758c6ef60b5fcbaceea29e0aa53
TimsalHassan <166032771+TimsalHassan@users.noreply.github.com>
2024-04-04 19:20:51 +0500

    Create At_even_index_string_finder.py

---
+++ At_even_index_string_finder.py
@@ -0,0 +1,13 @@
+
+
+#Name : Timsal Hassan
+#Roll Number : 1129
+#Semester : 1st 
+#Section : 3M
+while True:
+    str=input("Give your str: ")
+    for a in range(0,len(str)) :
+        if a %2==0:
+            print('\033[43m',str[a],'\033[0m')
+        else:
+            continue
